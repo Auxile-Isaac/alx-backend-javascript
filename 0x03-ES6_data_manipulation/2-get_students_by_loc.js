c4a7c375325dc338bdd27754217540609bfc142b
Auxile-Isaac <auxile.isaac@gmail.com>
2023-01-31 17:29:47 +0200

    2-get_students_by_loc.js modified

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -6,7 +6,7 @@
  *   location: String
  * }[]} students - The list of students.
  * @param {String} city - The location.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Auxile Isaac <https://github.com/Auxile-Isaac>
  * @returns
  */
 export default function getStudentsByLocation(students, city) {
