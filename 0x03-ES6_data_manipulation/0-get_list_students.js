7587de0055419d88f57cf6f3241ec0d7813c606c
Auxile-Isaac <auxile.isaac@gmail.com>
2023-01-31 17:18:31 +0200

    0-get_list_students modifications

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,6 +1,6 @@
 /**
  * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/B3zaleel>
+ * @author Auxile Isaac <https://github.com/Auxile-Isaac>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
 export default function getListStudents() {
